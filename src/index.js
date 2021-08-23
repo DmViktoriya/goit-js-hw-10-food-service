feffb7597cab66c5b9a85853d0f791d5f3406632
DmViktoriya <dmytrenkoviktoriya@gmail.com>
2021-08-23 21:07:34 +0300

    Added hbs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import "./js/themeSwitch"
+import { refs } from './js/refs.js';
+import menuTemplate from './templates/menu.hbs';
+import menu from './menu.json';
+import { getTheme } from './js/themeSwitch';
+import { switchTheme } from './js/themeSwitch';
+
+const markup = menuTemplate(menu);
+
+refs.menuRef.insertAdjacentHTML('beforeend', markup);
+
+getTheme();
+
+refs.switchRef.addEventListener('change', switchTheme);
